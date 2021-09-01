1adc78faa3f4bf16f66c6088352391e36a3da4f1
peterfhernandez <peter@hernandez.id.au>
2021-09-02 09:44:46 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Quick!
         </a>
       </header>
     </div>
